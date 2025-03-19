f8ed6fe18623a021f2d433fcbff8906a692fd52d
czh <2244356600@qq.com>
2025-03-19 18:26:00 +0800

    fix(global): fix spelling errors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-const cmd = reuqire('commander')
+const cmd = require('commander')
 
 const createCommands = require('./lib/core/create')
 const helpOptions = require('./lib/core/help')
 // Define the module's version number
-cmd.version(reuqire('./package.json').version)
+cmd.version(require('./package.json').version)
 
 // Add additional options to the help command
 helpOptions()
